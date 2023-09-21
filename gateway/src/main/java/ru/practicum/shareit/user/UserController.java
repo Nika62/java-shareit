1c8025c28f3315c3a08e66d50502635fc999a5a4
Nika <nika@w-up.ru>
2023-09-21 13:57:59 +0300

    fiat: add modules gateway, server. Attempt 2 style

--- gateway/src/main/java/ru/practicum/shareit/user/UserController.java
+++ gateway/src/main/java/ru/practicum/shareit/user/UserController.java
@@ -4,11 +4,17 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import ru.practicum.shareit.user.dto.UpdateEmail;
 import ru.practicum.shareit.user.dto.UserDto;
 
-import java.util.List;
 
 @RestController
 @RequestMapping(path = "/users")
